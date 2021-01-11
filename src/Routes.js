d7210f4c41e99a93aa673c471830ed4037754fe7
Chris Pooley <cpooley@gmail.com>
2021-01-11 13:31:35 -0800

    Completed React Template

--- src/Routes.js
+++ src/Routes.js
@@ -2,3 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const routes = [ ];
+
+export const Routes = () => (
+  <Router>
+    <Switch>
+      {routes.map((route, index) => (
+        <Route
+          key={index}
+          path={routes.path}
+          exact={route.exact}
+          >
+            <route.Component />
+          </Route>
+      ))}
+    </Switch>
+  </Router>
+)
